da87070ed82c8fe9b5f34f8745e49204f6ac8040
Gidraf Orenjaa Mtange <gmtange@safaricom.co.ke>
2026-03-19 21:10:11 +0300

    Fix build

--- app/views/auth/__init__.py
+++ app/views/auth/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-authorization = Blueprint('auth', __name__, url_prefix='/api/auth')
+authorization = Blueprint('auth', __name__, url_prefix='/api/auth/admin')
 
 from . import auth_admin_view
