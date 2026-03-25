1665e0c1476138ec5f24bfb7a9b3456d8f9071de
Gidraf Orenjaa Mtange <gmtange@safaricom.co.ke>
2026-03-25 21:20:03 +0300

    Fix feedback feedback

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
-
+from app.workers.sp_live_harvester import start_harvester_threa, start_harvester_thread
 from app.extensions import db, init_celery, jwt, socketio, migrate, cors
 from app.views.onboarding.playwright_onboarding import bp_fetcher, init_fetcher_manager
 
@@ -109,7 +109,7 @@ def create_app() -> Flask:
     flask_app.register_blueprint(bp_interceptor)
     flask_app.register_blueprint(bp_sp)
     flask_app.register_blueprint(bp_sp_live)
-
+    start_harvester_thread()
     # init_playwright_manager()
     init_fetcher_manager()
 
